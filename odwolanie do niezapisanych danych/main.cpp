1f6e6da6ab71fa4e609e89a77edc993e1c97dd57
dbracki <danielb0498@wp.pl>
2018-03-14 22:12:21 +0100

    init comit

--- odwolanie do niezapisanych danych/main.cpp
+++ odwolanie do niezapisanych danych/main.cpp
@@ -4,14 +4,8 @@ using namespace std;
 
 int main()
 {
-    int t[10], a;
-        for (int i=0; i<10; i++){
-        t[i]=i;
-    }
-    a=1;
-    while (a>0){
-        cout<<t[a]<<endl;
-        a++;
-    }
+    int a;
+    cout<<a<<endl;
+
     return 0;
 }
